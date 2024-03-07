0ae4a345c0e049a218bbec3d59ba3359c90099ba
Vineeth2496 <vineetharmoori2496@gmail.com>
2024-03-08 02:25:31 +0530

    Initial commit

---
+++ src/main/java/com/SpringBootProfileTestYAML/SpringBootProfileTestYamlApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootProfileTestYAML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfileTestYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfileTestYamlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootProfileTestYAML/SpringBootProfileTestYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootProfileTestYAML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProfileTestYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
